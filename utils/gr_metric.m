e13a8562082daa050963b9cd1d28ee59a4e80309
Rémi G. <48289134+rgiraud@users.noreply.github.com>
2025-04-28 11:01:44 +0200

    Add files via upload

--- utils/gr_metric.m
+++ utils/gr_metric.m
@@ -1,11 +1,10 @@
 %
 % Input:    lab_map - Labeling decomposition map of a 2D image
-% Outputs:  gr - Global Regularity measure that evaluates the global regularity 
+% Outputs:  gr - Global Regularity measure that evaluates the global regularity
 %           src - Shape Regularity Criteria that evaluates the local shape regularity
 %           smf - Smooth Matching Factor that evaluates the shape consistency over the decomposition
 
-
-function [gr,src,smf] = gr_metric(lab_map)
+function [gr,src,smf] = eval_gr(lab_map)
 
 
 [h,w]  = size(lab_map);
@@ -24,29 +23,44 @@ for k=sp_ind
     [yk,xk]  = find(S_k);
     size_S_k = length(yk);
     
-    %Convex hull of S_k
-    hull       = regionprops(S_k,'ConvexImage');
-    hull       = hull.ConvexImage;
-    perim_hull = regionprops(hull,'Perimeter');
-    perim_hull = perim_hull.Perimeter;
-    cc_hull    = perim_hull/sum(hull(:));
+    %If non connected components -> Take largest blob
+    ll = bwlabel(S_k, 8);
+    size_n = zeros(length(unique(ll(:))),1);
+    for n=0:length(unique(ll(:)))-1
+        size_n(n+1) = sum(sum(ll==n));
+    end
+    [~,max_n] = sort(size_n,'descend');
+    S_k = ll==(max_n(2)-1);
+    size_S_k = sum(S_k(:)==1);
     
-    perim_S_k = regionprops(S_k,'Perimeter');
-    perim_S_k = perim_S_k.Perimeter;
-    cc_S_k    = perim_S_k/sum(S_k(:));
-    
-    %Evaluates the convexity of S_k
-    cr_k = cc_hull/cc_S_k; 
-    
-    %Evaluates the balanced repartition of S_k
-    sigma_x = std(xk(:));
-    sigma_y = std(yk(:));
-    vxy_k   = sqrt(min(sigma_x,sigma_y)/max(sigma_x,sigma_y)); 
-    
-    %Shape Regularity Criteria (SRC)
-    src_k = cr_k*vxy_k;
-    src   = src + src_k*size_S_k;
-    
+    if (size_S_k > 1)
+        %Convex hull of S_k
+        hull       = regionprops(S_k,'ConvexImage');
+        hull       = hull.ConvexImage;
+        perim_hull = regionprops(hull,'Perimeter');
+        perim_hull = perim_hull.Perimeter;
+        cc_hull    = perim_hull/sum(hull(:));
+        
+        perim_S_k = regionprops(S_k,'Perimeter');
+        perim_S_k = perim_S_k.Perimeter;
+        cc_S_k    = perim_S_k/sum(S_k(:));
+        
+        %Evaluates the convexity of S_k
+        cr_k = cc_hull/cc_S_k;
+        
+        %Evaluates the balanced repartition of S_k
+        sigma_x = std(xk(:));
+        sigma_y = std(yk(:));
+        vxy_k   = sqrt(min(sigma_x,sigma_y)/max(sigma_x,sigma_y));
+        
+        %Shape Regularity Criteria (SRC)
+        src_k = cr_k*vxy_k;
+        if (isnan(src_k))
+            src_k
+            k
+        end
+        src   = src + src_k*size_S_k;
+    end
     
 end
 src = src/(h*w);
@@ -56,7 +70,7 @@ src = src/(h*w);
 %% Shape consistency evaluation
 
 %To store the registered shapes
-S_tab = zeros(2*h+1,2*w+1,sp_nbr);
+S_tab = zeros(2*h+1,2*w+1);
 
 %Average of superpixel shapes
 c = 0;
@@ -69,25 +83,36 @@ for k=sp_ind
     %Barycenter
     my = round(mean(yk(:)));
     mx = round(mean(xk(:)));
-
     
     for l=1:length(yk)
         %Registered position
         yk_r               = yk(l)+(h+1-my);
         xk_r               = xk(l)+(w+1-mx);
-        S_tab(yk_r,xk_r,c) = S_tab(yk_r,xk_r,c) + 1;
+        S_tab(yk_r,xk_r) = S_tab(yk_r,xk_r) + 1;
     end
     
     
 end
-S = sum(S_tab,3)/sp_nbr;
+S = S_tab/sp_nbr;
 S = S/sum(S(:));
 
 %Smooth Matching Factor (SMF)
 smf = 0;
 sum_smf = 0;
+
+
 for k=1:c
-    S_k      = S_tab(:,:,k);
+    S_k = zeros(2*h+1,2*w+1);
+    [yk,xk] = find(lab_map == k);
+    my = round(mean(yk(:)));
+    mx = round(mean(xk(:)));
+    for l=1:length(yk)
+        %Registered position
+        yk_r               = yk(l)+(h+1-my);
+        xk_r               = xk(l)+(w+1-mx);
+        S_k(yk_r,xk_r) = S_k(yk_r,xk_r) + 1;
+    end
+    
     size_S_k = sum(S_k(:));
     S_k      = S_k/size_S_k;
     smf_tmp = sum(sum(abs(S-S_k)))/2;
@@ -104,4 +129,5 @@ gr = src*smf;
 
 
 end
+
 
